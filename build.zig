a88abababdea0d3cf00bf6731844d72ee39e9e03
Tesseract22 <tesseractcat@proton.me>
2025-08-26 10:10:27 +0000

    [CHORE] Refactor wasm building logic

--- build.zig
+++ build.zig
@@ -1,13 +1,88 @@
 const std = @import("std");
+const Build = std.Build;
 
-fn compile_dir_wasm(b: *std.Build,
-    step: *std.Build.Step,
+pub fn get_wasm_include_from_sysroot(b: *Build) Build.LazyPath {
+    if (b.sysroot == null) {
+        @panic("Pass '--sysroot \"$EMSDK/upstream/emscripten\"'");
+    }
+
+    const cache_include = std.fs.path.join(b.allocator, &.{ b.sysroot.?, "cache", "sysroot", "include" }) catch @panic("Out of memory");
+
+    var dir = std.fs.openDirAbsolute(cache_include, .{ .access_sub_paths = true, .no_follow = true }) catch @panic("No emscripten cache. Generate it!");
+    dir.close();
+
+    return .{ .cwd_relative = cache_include };
+}
+
+pub fn create_wasm_mod(
+    b: *Build,
+    optimize: std.builtin.OptimizeMode,
+    mod_name: []const u8,
+    src_path: Build.LazyPath,
+) *Build.Module {
+    const target = b.resolveTargetQuery(.{
+        .cpu_arch = .wasm32,
+        .cpu_model = .{ .explicit = &std.Target.wasm.cpu.mvp },
+        .cpu_features_add = std.Target.wasm.featureSet(&.{
+            .atomics,
+            .bulk_memory,
+        }),
+        .os_tag = .emscripten,
+    });
+    const mod = b.addModule(mod_name, .{
+        .root_source_file = src_path,
+        .target = target,
+        .optimize = optimize,
+    });
+    return mod;
+}
+
+pub fn compile_to_wasm(
+    b: *Build,
+    module: *Build.Module,
+    mod_name: []const u8) *Build.Step {
+
+    // This create an archive that contains a .o file. This .o file is NOT an ELF.
+    // Instead, it is a wasm binary module.
+    // emcc can directly handle this kind of archive file.
+    const wasm_module = b.addLibrary(.{
+        .linkage = .static,
+        .root_module = module,
+        .name = mod_name
+    });
+    wasm_module.linkLibC();
+    // wasm.addIncludePath(.{ .cwd_relative = cache_include });
+    wasm_module.entry = .disabled; // for some reason it still export main in the std/start.zig ?? and this does not help anything
+    wasm_module.rdynamic = true;
+    wasm_module.bundle_ubsan_rt = false;
+    wasm_module.bundle_compiler_rt = false;
+
+    // const arti = b.addInstallArtifact(wasm_module, .{});
+    // step.dependOn(&arti.step);
+    const emcc = b.addSystemCommand(&.{"emcc", "-sMODULARIZE=1", "-sEXPORT_ES6=1", "-Oz"});
+    emcc.addArtifactArg(wasm_module);
+    emcc.addArg("-o");
+    const wasm = emcc.addOutputFileArg(b.fmt("{s}.mjs", .{mod_name}));
+
+    const install_wasm = b.addInstallDirectory(.{
+        .source_dir = wasm.dirname(),
+        .install_dir = .{ .custom = "web" },
+        .install_subdir = ".",
+    });
+
+    install_wasm.step.dependOn(&emcc.step);
+    return &emcc.step;
+
+}
+
+fn compile_dir_wasm(b: *Build,
+    step: *Build.Step,
     opt: std.builtin.OptimizeMode,
     path: []const u8,
     enable_target_suffix: bool,
     prefix_filter_opt: ?[]const u8,
-    zynth: *std.Build.Module,
-    preset: *std.Build.Module) !void {
+    zynth: *Build.Module,
+    preset: *Build.Module) !void {
     const target = b.resolveTargetQuery(.{
         .cpu_arch = .wasm32,
         .cpu_model = .{ .explicit = &std.Target.wasm.cpu.mvp },
@@ -34,48 +109,13 @@ fn compile_dir_wasm(b: *std.Build,
         const exe_name = if (!enable_target_suffix) stripped else b.fmt("{s}-{s}-{s}-{s}", 
             .{stripped, @tagName(target.result.cpu.arch), @tagName(target.result.abi), @tagName(target.result.os.tag)});
 
-        const mod = b.addModule(file.name, .{
-            .root_source_file = b.path(b.fmt("{s}/{s}", .{path, file.name})),
-            .target = target,
-            .optimize = opt,
-        });
+        const mod = create_wasm_mod(b, opt, file.name, b.path(b.fmt("{s}/{s}", .{path, file.name})));
         mod.addImport("zynth", zynth);
         mod.addImport("preset", preset);
 
-        const wasm_module = b.addLibrary(.{
-            .linkage = .static,
-            .root_module = mod,
-            .name = exe_name
-        });
-        wasm_module.linkLibC();
-        // wasm.addIncludePath(.{ .cwd_relative = cache_include });
-        wasm_module.entry = .disabled; // for some reason it still export main in the std/start.zig ?? and this does not help anything
-        wasm_module.rdynamic = true;
-        wasm_module.bundle_ubsan_rt = false;
-        wasm_module.bundle_compiler_rt = false;
+        const wasm_step = compile_to_wasm(b, mod, file.name); 
+        step.dependOn(wasm_step);
 
-        //
-        // run emcc on the static lib, and install the generated *.mjs and *.wasm
-        //
-        const arti = b.addInstallArtifact(wasm_module, .{});
-        // step.dependOn(&arti.step);
-        const emcc = b.addSystemCommand(&.{"emcc", "-sMODULARIZE=1", "-sEXPORT_ES6=1", "-Oz"});
-        emcc.addArtifactArg(wasm_module);
-        emcc.addArg("-o");
-        const wasm = emcc.addOutputFileArg(b.fmt("{s}.mjs", .{exe_name}));
-
-        const install_wasm = b.addInstallDirectory(.{
-            .source_dir = wasm.dirname(),
-            .install_dir = .{ .custom = "web" },
-            .install_subdir = ".",
-        });
-
-        install_wasm.step.dependOn(&arti.step);
-        emcc.step.dependOn(&arti.step);
-        step.dependOn(&emcc.step);
-        step.dependOn(&install_wasm.step);
-
-        
         if (first) try wasm_manifest.writer.print("\"{s}\"", .{exe_name})
         else try wasm_manifest.writer.print(", \"{s}\"", .{exe_name});
 
@@ -188,21 +228,9 @@ pub fn build(b: *std.Build) !void {
     });
     preset.addImport("zynth", zynth);
     if (target.result.cpu.arch.isWasm()) {
-        if (b.sysroot == null) {
-            @panic("Pass '--sysroot \"$EMSDK/upstream/emscripten\"'");
-        }
-
-        const cache_include = std.fs.path.join(b.allocator, &.{ b.sysroot.?, "cache", "sysroot", "include" }) catch @panic("Out of memory");
-        defer b.allocator.free(cache_include);
-
-        var dir = std.fs.openDirAbsolute(cache_include, .{ .access_sub_paths = true, .no_follow = true }) catch @panic("No emscripten cache. Generate it!");
-        dir.close();
-
-        ma.addIncludePath(.{ .cwd_relative = cache_include });
-        zynth.addIncludePath(.{ .cwd_relative = cache_include });
-
-        const zemscripten = b.dependency("zemscripten", .{});
-        zynth.addImport("zemscripten", zemscripten.module("root"));
+        const libc_path = get_wasm_include_from_sysroot(b);
+        zynth.addIncludePath(libc_path);
+        ma.addIncludePath(libc_path);
     }
 
     const prefix_filter_opt = b.option([]const u8, "example-filter", "filter the the examples to build with the prefix of the provided strings.");
@@ -212,63 +240,4 @@ pub fn build(b: *std.Build) !void {
 
     try compile_dir(b, example_step, target, opt, "src/examples", enable_target_suffix, prefix_filter_opt, zynth, preset);
     if (enable_gui) try compile_dir(b, example_step, target, opt, "src/examples/gui", enable_target_suffix, prefix_filter_opt, zynth, preset);
-
-
-    //     const wasm_target = b.resolveTargetQuery(.{.cpu_arch = .wasm32, .os_tag = .emscripten});
-    //     std.log.debug("wasm target {}", .{wasm_target});
-    // 
-    //     const zynth_wasm = b.addModule("zynth", .{
-    //         .root_source_file = b.path("src/wasm_demo.zig"),
-    //         .target = target,
-    //         .optimize = opt,
-    //         .link_libc = enable_gui,
-    //     });
-
-    //     const activate_emsdk_step = @import("zemscripten").activateEmsdkStep(b);
-    // 
-    //     const zemscripten = b.dependency("zemscripten", .{});
-    //     wasm.root_module.addImport("zemscripten", zemscripten.module("root"));
-    // 
-    //     const emcc_flags = @import("zemscripten").emccDefaultFlags(b.allocator, .{
-    //         .optimize = opt,
-    //         .fsanitize = false,
-    //     });
-    //     
-    //     var emcc_settings = @import("zemscripten").emccDefaultSettings(b.allocator, .{
-    //         .optimize = opt,
-    //     });
-    // 
-    //     try emcc_settings.put("ALLOW_MEMORY_GROWTH", "1");
-    // 
-    //     const emcc_step = @import("zemscripten").emccStep(
-    //         b,
-    //         wasm,
-    //         .{
-    //             .optimize = opt,
-    //             .flags = emcc_flags,
-    //             .settings = emcc_settings,
-    //             .use_preload_plugins = true,
-    //             .embed_paths = &.{},
-    //             .preload_paths = &.{},
-    //             .install_dir = .{ .custom = "web" },
-    //         },
-    //     );
-    //     emcc_step.dependOn(activate_emsdk_step);
-    // 
-    //     b.getInstallStep().dependOn(emcc_step);
-    //     //compile_dir(b, wasm_target, opt, "src/examples", enable_target_suffix, prefix_filter_opt, zynth, preset);
-    //     
-    //     const html_filename = try std.fmt.allocPrint(b.allocator, "{s}.html", .{wasm.name});
-    // 
-    //     const emrun_args = .{};
-    //     const emrun_step = @import("zemscripten").emrunStep(
-    //         b,
-    //         b.getInstallPath(.{ .custom = "web" }, html_filename),
-    //         &emrun_args,
-    //     );
-    // 
-    //     emrun_step.dependOn(emcc_step);
-    // 
-    //     b.step("emrun", "Build and open the web app locally using emrun").dependOn(emrun_step);
-    // 
 }
